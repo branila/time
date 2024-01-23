c3fcbb2d5632e6d6124881b311fd5c84cb56e8a1
branilaa <stefan.branila55@gmail.com>
2024-01-23 17:11:56 +0100

    Changed database management system from OOP to procedural

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,20 +1,18 @@
 import { MONGO_URL } from '$env/static/private'
 import { MongoClient } from 'mongodb'
 
-class Database {
-  static client = new MongoClient(MONGO_URL)
-  static users = this.client.db('time').collection('users')
-  static history = this.client.db('time').collection('history')
-  static events = this.client.db('time').collection('events')
+const client = new MongoClient(MONGO_URL)
+const users = client.db('time').collection('users')
+const history = client.db('time').collection('history')
+const events = client.db('time').collection('events')
 
-  static async connect() {
-    try {
-      await this.client.connect()
-      console.log('Successfull database connection')
-    } catch (err) {
-      console.error('Database connection error: ', err)
-    }
+export default async function connect() {
+  try {
+    await client.connect()
+    console.log('Successfull database connection')
+  } catch (err) {
+    console.error('Database connection error: ', err)
   }
 }
 
-export default Database
+export { users, history, events }
