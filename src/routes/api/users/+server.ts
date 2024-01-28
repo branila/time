1b9b9da56670e6740b4e1f5487d6d10c3b0c09cf
branilaa <stefan.branila55@gmail.com>
2024-01-28 16:07:45 +0100

    Created user management API

---
+++ src/routes/api/users/+server.ts
@@ -0,0 +1,64 @@
+import { json, error } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+import { users } from '$db'
+import { ObjectId } from 'mongodb'
+
+export const GET: RequestHandler = async ({ locals }) => {
+  if (!locals.user || locals.user.role !== 'admin') {
+    return error(401, 'Unauthorized')
+  }
+
+  return json(await users.find().toArray())
+}
+
+export const POST: RequestHandler = async ({ request, locals }) => {
+  if (!locals.user || locals.user.role !== 'admin') {
+    return error(401, 'Unauthorized')
+  }
+
+  const { email, password, role } = await request.json()
+
+  if (!email || !password || !role) {
+    return error(400, 'Bad request: missing fields')
+  }
+
+  await users.insertOne({ email, password, role })
+
+  return json({ message: 'ok' })
+}
+
+export const DELETE: RequestHandler = async ({ request, locals }) => {
+  if (!locals.user || locals.user.role !== 'admin') {
+    return error(401, 'Unauthorized')
+  }
+
+  const { id } = await request.json()
+  
+  if (!id) {
+    return error(400, 'Bad request: missing fields')
+  }
+
+  const userId = new ObjectId(id)
+
+  await users.deleteOne({ _id: userId })
+
+  return json({ message: 'ok' })
+}
+
+export const PUT: RequestHandler = async ({ request, locals }) => {
+  if (!locals.user || locals.user.role !== 'admin') {
+    return error(401, 'Unauthorized')
+  }
+
+  const { id, email, password, role } = await request.json()
+  
+  if (!id || !email || !password || !role) {
+    return error(400, 'Bad request: missing fields')
+  }
+  
+  const userId = new ObjectId(id)
+  
+  await users.updateOne({ _id: userId }, { $set: { email, password, role } })
+
+  return json({ message: 'ok' })
+}
