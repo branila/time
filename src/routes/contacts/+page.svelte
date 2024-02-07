ac37f6bb9da553a61ebe3207915bd8bc180c0d50
branilaa <stefan.branila55@gmail.com>
2024-02-07 13:12:45 +0100

    More refactoring stuff, minor changes

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,4 +1,3 @@
-<!-- TODO: Contacts page customization -->
 <h1 class="flex pb-36 justify-center items-center h-screen text-8xl">
   Contatti!
 </h1>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,3 @@
-<script>
-  export let data;
-</script>
-
 <section class="mx-[5%] md:mx-[20%] relative">
 
 </section>
